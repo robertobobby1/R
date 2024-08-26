0c6f4f174f2bc640178302ea961e8d4c1b4570ef
Roberto Milla Martinez <roberto@accounton.io>
2024-08-26 02:03:13 +0200

    fix: problem with enum redefinitions

--- R/Source/Net/P2P.h
+++ R/Source/Net/P2P.h
@@ -25,12 +25,12 @@ namespace R::Net::P2P {
         ServerClientHeaderFlags_Action = 1 << 7,  // 10000000
     };
 
-    enum LobbyPrivacyType {
+    enum class LobbyPrivacyType {
         Private,
         Public
     };
 
-    enum ClientActionType {
+    enum class ClientActionType {
         Create,
         Connect,
         Disconnect,
